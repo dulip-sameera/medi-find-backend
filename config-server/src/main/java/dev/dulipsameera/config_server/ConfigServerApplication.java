6160a261da86edaeb3b0e985c94452fcd6402b87
Dulip Sameeera <dsgitrepo101@gmail.com>
2026-04-07 14:48:07 +0530

    configure the config server

--- config-server/src/main/java/dev/dulipsameera/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/dev/dulipsameera/config_server/ConfigServerApplication.java
@@ -2,12 +2,15 @@ package dev.dulipsameera.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+		
 	}
 
 }
