eb5001dfbc55f7a045be9b4a78c02966d226f6b7
Dulip Sameeera <dsgitrepo101@gmail.com>
2026-04-08 03:18:02 +0530

    create a test endpoint

---
+++ auth-service/src/main/java/dev/dulipsameera/auth_service/controller/AuthController.java
@@ -0,0 +1,16 @@
+package dev.dulipsameera.auth_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "Auth Service is LIVE! Please provide credentials.";
+    }
+
+}
